d7410619f915a8f5fd5bf653c4a69944d1da3cdd
DON <tochey@outlook.com>
2023-11-30 22:37:22 -0800

    Refactor server boot process to handle SIGTERM signal gracefully
    This change updates the server boot process to handle the SIGTERM signal
    gracefully. When a SIGTERM signal is received, the server now logs the
    signal, performs cleanup tasks, and then exits. This ensures proper
    shutdown of the server process and prevents potential resource leaks.
    
    The previous implementation already handled the SIGTERM signal, but it
    was duplicated in code. The duplication has been removed, and the
    exiting logic has been consolidated into a single location.
    
    This change improves the overall stability and reliability of the server
    by properly handling termination signals.
    
    No issue reference.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,6 +3,17 @@ import app from "./app";
 import { Server as httpserver } from "http";
 
 async function bootServer(PORT: number): Promise<httpserver> {
+  process.on('SIGTERM', async () => {
+    console.info('[express] SIGTERM received');
+  
+    console.info('[express] cleaning up');
+    await new Promise(resolve => setTimeout(resolve, 100));
+  
+    console.info('[express] exiting');
+    process.exit(0)
+  });
+
+  
   return app.listen(PORT, () => {
     console.log(`Server listening in ${process.env.NODE_ENV} mode`);
   });
@@ -10,13 +21,3 @@ async function bootServer(PORT: number): Promise<httpserver> {
 
 const PORT = parseInt(process.env.PORT || "8080", 10);
 bootServer(PORT);
-
-process.on('SIGTERM', async () => {
-  console.info('[express] SIGTERM received');
-
-  console.info('[express] cleaning up');
-  await new Promise(resolve => setTimeout(resolve, 100));
-
-  console.info('[express] exiting');
-  process.exit(0)
-});
